660c3c1bb566e533c326fd826aadff94b49938db
Hong An <anhong2017@gmail.com>
2020-05-27 20:24:40 -0500

    Create get_matched_annot.sh

---
+++ get_matched_annot.sh
@@ -0,0 +1,12 @@
+###Usage: sh get_matched_annot.sh sample.name
+
+while read line
+do
+        sbatch -n 8 --mem 64G -t 2:00:00 --wrap="cd '$line'_spades; \
+        perl ../get_seq300.pl scaffolds.fasta scaffolds_300.fa; \
+        makeblastdb -in scaffolds_300.fa -dbtype nucl -hash_index -parse_seqids; \
+        blastn -query ../Ath_cp_gene.fa -db scaffolds_300.fa -evalue 1E-5 -outfmt 6 -num_threads 12 -out '$line'_ath.annot; \
+        perl ../get_matched_cpGene.pl ../Ath_cp_len.txt '$line'_ath.annot '$line'_ath_matched.annot 0; \
+        cp '$line'_ath_matched.annot ../matched_annot; \
+        cd ../"
+done<$1
