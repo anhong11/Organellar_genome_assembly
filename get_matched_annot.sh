eed2ddc8023aae93762e495d1af582f7af9834a9
Hong An <anhong2017@gmail.com>
2020-05-27 20:40:26 -0500

    Update get_matched_annot.sh

--- get_matched_annot.sh
+++ get_matched_annot.sh
@@ -6,7 +6,7 @@ do
         perl ../get_seq300.pl scaffolds.fasta scaffolds_300.fa; \
         makeblastdb -in scaffolds_300.fa -dbtype nucl -hash_index -parse_seqids; \
         blastn -query ../Ath_cp_gene.fa -db scaffolds_300.fa -evalue 1E-5 -outfmt 6 -num_threads 12 -out '$line'_ath.annot; \
-        perl ../get_matched_cpGene.pl ../Ath_cp_len.txt '$line'_ath.annot '$line'_ath_matched.annot 0; \
+        perl ../get_matched_cpGene.pl ../Ath_cp_len.txt '$line'_ath.annot '$line'_ath_matched.annot 2; \
         cp '$line'_ath_matched.annot ../matched_annot; \
         cd ../"
 done<$1
